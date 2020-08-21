d598139ec46a26db9c1722964fc06de9c7535913
IamRakibAhmed <rakibofficial@gmail.com>
2020-08-22 05:25:43 +0600

    Calculating Function

---
+++ Codeforces/Practice/486A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ll n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << n / 2 << endl;
+    else 
+        cout << ((n / 2) + 1) * -1 << endl;
+    
+    return 0;
+}
