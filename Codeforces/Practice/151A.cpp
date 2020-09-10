6b5c8586a005e96b51a06f184d4de85a881f5859
Rakib Ahmed <rakibofficial@gmail.com>
2020-09-10 09:26:54 +0600

    Soft Drinking

---
+++ Codeforces/Practice/151A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+
+    int drink = (k * l) / nl;
+    int lime = c * d;
+    int salt = p / np;
+
+    int res = min(drink, min(lime, salt)) / n;
+    cout << res << endl;
+
+    return 0;
+}
