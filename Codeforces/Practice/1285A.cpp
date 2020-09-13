5b942d7108d1d7f75120d50ad6f5f0ccd6c648d7
Rakib Ahmed <rakibofficial@gmail.com>
2020-09-14 02:29:56 +0600

    Mezo Playing Zoma

---
+++ Codeforces/Practice/1285A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    string str;
+    cin >> str;
+
+    cout << n + 1 << endl;
+
+    return 0;
+}
