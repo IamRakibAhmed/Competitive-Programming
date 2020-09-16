fd0f56cfce08311e249c4087e2165fdeb51b4e04
Rakib Ahmed <rakibofficial@gmail.com>
2020-09-16 19:21:35 +0600

    Prime Subtraction

---
+++ Codeforces/Practice/1238A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        ll x, y;
+        cin >> x >> y;
+
+        x -= y;
+        if (x == 1) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+
+    return 0;
+}
