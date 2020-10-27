67f9289798d84387e13fc226858930e81d02861f
Rakib Ahmed <rakibofficial@gmail.com>
2020-10-28 01:40:53 +0600

    Marketing Scheme

---
+++ Codeforces/Practice/1437A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        if (a * 2 > b) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
+}
