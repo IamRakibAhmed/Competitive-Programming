444e8e6f213bc15fcad17f4e22517d91c7a0ce44
Rakib Ahmed <rakibofficial@gmail.com>
2021-02-05 07:05:16 +0600

    Lucky Division

---
+++ Codeforces/Practice/122A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    fast_io
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+
+    if (n % 4 == 0 || n % 7 == 0 || n % 44 == 0 || n % 47 == 0 || n % 444 == 0 || n % 447 == 0 || n % 477 == 0 || n % 77 == 0 || n % 74 == 0 || n % 777 == 0 || n % 774 == 0 || n % 474 == 0 || n % 747 == 0 || n % 744 == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
