f136c6b6c765d5c4f65e484907873ea6f74e5ec2
Rakib Ahmed <rakibofficial@gmail.com>
2021-02-28 15:10:20 +0600

    Arena

---
+++ Codeforces/Practice/1487A.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    fast_io
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        sort(a.begin(), a.end());
+        cout << n - count(a.begin(), a.end(), a[0]) << endl;
+    }
+
+    return 0;
+}
